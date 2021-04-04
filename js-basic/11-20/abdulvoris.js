b6b18dcf1e5f4eab6078287e18574e83916c6a34
Abdulvorisjs <abdulvoris000urolov>
2021-04-04 11:49:11 +0500

    11-20

---
+++ js-basic/11-20/abdulvoris.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>5 vazifa</title>
+</head>
+<body>
+<script>
+
+var a = 4;
+var b = 5;
+var c = `Abdulvoris`;
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+alert(a);
+alert(b);
+alert(c);
+</script>
+</body>
+</html>

796fab2bc231029b67f010efae5148a410470e58
Abdulvorisjs <abdulvoris000urolov>
2021-05-26 12:31:37 +0500

    project

---
+++ project/DarkWeb_BackEnd/src/routes/user.js
@@ -0,0 +1,12 @@
+const express = require(`express`);
+const router = express.Router();
+
+
+router.post(`/signup`, signup);
+router.post(`/signin`, (req, res) => {
+
+   
+
+});
+
+module.exports = router;
